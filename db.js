4a387b3f69b1965acf24669237b5c9446e9db883
Diogo Travalha <diogotravalha@gmail.com>
2022-02-19 15:35:59 -0300

    19-02-2022

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-    user: 'rwfbqgqrwmpanc',
-    password:
-        '0da229cecdc6d2dc89eda70676433959cb42e94f60c49e39f2ab1bc122138fd1',
-    host: 'ec2-54-209-221-231.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'd2018243ih0upg',
-});
-
 // const pool = new Pool({
-//     user: 'postgres',
-//     password: 'Glock9mm',
-//     host: 'localhost',
-//     port: 5433,
-//     database: 'perntodo',
+//     user: 'rwfbqgqrwmpanc',
+//     password:
+//         '0da229cecdc6d2dc89eda70676433959cb42e94f60c49e39f2ab1bc122138fd1',
+//     host: 'ec2-54-209-221-231.compute-1.amazonaws.com',
+//     port: 5432,
+//     database: 'd2018243ih0upg',
 // });
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'Glock9mm',
+    host: 'localhost',
+    port: 5433,
+    database: 'perntodo',
+});
 
 module.exports = pool;
