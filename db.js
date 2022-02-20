f3f32f96bb3f13eaf5479b37173d3f139703a35f
Diogo Travalha <diogotravalha@gmail.com>
2022-02-20 11:26:15 -0300

    19-02-2022

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     password: 'Glock9mm',
-    host: 'http://187.85.170.204:3333',
+    host: 'http://187.85.170.204',
     port: 5433,
     database: 'perntodo',
 });
